d81a1fd97a6cc64a5c3a4ce7ffeb6eb219b44970
aleger12@yandex.ru <Aleger19791989>
2021-08-27 16:26:01 +0500

    создание машины

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -1,4 +1,59 @@
 package hiber.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Cars")
 public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String model;
+
+    @Column
+    private int series;
+
+    @OneToOne
+    @JoinColumn
+    private User user;
+
+    public Car(){
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public int getSeries(){
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public User getUser(){
+        return user;
+    }
+
+    public User setUser(User user) {
+        this.user = user;
+        return user;
+    }
+
 }
