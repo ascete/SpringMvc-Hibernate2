a8fe6050a3d83e69691b4c818efb30a928ce93e0
Андрей Александрович Белицкий <aleger12@yandex.ru>
2021-09-03 16:25:12 +0500

    Add files via upload

---
+++ Mvc-Hibernate/SpringMvc-Hibernate/src/main/java/dao/DaoUser.java
@@ -0,0 +1,15 @@
+package dao;
+
+public interface UserDao {
+    void createUsersTable();
+
+    void dropUsersTable();
+
+    void saveUser(String name, String lastName, byte age);
+
+    void removeUserById(long id);
+
+    List<User> getAllUsers();
+
+    void cleanUsersTable();
+}
